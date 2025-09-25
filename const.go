0e9a7e9508a5c7c0107aa2475b7e6770e25857c5
Sam Anthony <sam@samanthony.xyz>
2025-09-25 11:03:52 -0400

    vacuum permettivity constant

--- const.go
+++ const.go
@@ -2,12 +2,19 @@ package main
 
 import "math"
 
+const (
+	// Vacuum permettivity [C²/(N·m²)]
+	e0 = 8.8541878188e-12
+)
+
 func parseConstant(s string) (float64, error) {
 	switch s {
 	case "pi":
 		return math.Pi, nil
 	case "e":
 		return math.E, nil
+	case "e0":
+		return e0, nil
 	}
 	return 0, InvalidConstantErr{s}
 }
